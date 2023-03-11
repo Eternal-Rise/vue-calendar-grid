9857e05c6a72ea7bb287e83bb6a47dccc557584a
Eternal-Rise <my_dawn@ukr.net>
2023-03-11 18:05:59 +0200

    feat: add week grid

--- index.js
+++ index.js
@@ -7,43 +7,33 @@ import localeData from 'dayjs/esm/plugin/localeData';
 import weekOfYear from 'dayjs/esm/plugin/weekOfYear';
 
 dayjs.extend(isLeapYear);
-// dayjs.extend(isoWeek);
+dayjs.extend(isoWeek);
 dayjs.extend(isoWeeksInYear);
-// dayjs.extend(localeData);
-// dayjs.extend(weekOfYear);
+dayjs.extend(localeData);
+dayjs.extend(weekOfYear);
 
-const getWeekGrid = (date) => {
-  const startDate = dayjs(date).startOf('week');
-  const weekNumber = date.isoWeek();
+const getWeekGrid = (d) => {
+  const date = dayjs(d);
+  const startDate = date.startOf('week');
+  const week = date.isoWeek();
   const days = [];
 
   for (let i = 0; i < 7; i++) {
     const date = startDate.add(i, 'day');
     days.push({
-      date: date.format('YYYY-MM-DD'),
-      number: date.date(),
+      date: date.date(),
+      dateString: date.format('YYYY-MM-DD'),
     });
   }
 
-  return { weekNumber, days };
+  return { week, days };
 };
 
 export const useCalendarGrid = (defaultValues = {}) => {
   const date = ref(defaultValues.date ?? new Date());
-  const weekStart = ref(defaultValues.weekStart ?? 0);
 
   const weekdays = computed(() => {
-    const daysNames = dayjs.weekdays();
-    const days = [];
-
-    let day = weekStart.value;
-    for (let i = 0; i < 7; i += 1) {
-      days.push(daysNames[day]);
-      day += 1;
-      if (day > 6) day = 0;
-    }
-
-    return days;
+    return dayjs.weekdays();
   });
 
   const weekGrid = computed(() => getWeekGrid(date.value));
